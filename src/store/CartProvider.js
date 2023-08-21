1135289baec882a8a2de11fb00eaa969506f7d8a
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-08-21 10:31:31 +0500

    Cart Context Added

---
+++ src/store/CartProvider.js
@@ -0,0 +1,47 @@
+import React, { useReducer } from "react";
+import CartContext from "./cart-context";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedAmount,
+    };
+  }
+  if (action.type === "REMOVE") {
+  }
+  return defaultCartState;
+};
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+  const cartContextObj = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContextObj}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+export default CartProvider;
