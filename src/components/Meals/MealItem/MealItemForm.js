f6b1134d5976f712daba90f193f07a9353466149
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-08-23 19:51:50 +0500

    MealItem input or form is working

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,21 +1,41 @@
-import Input from '../../UI/Input';
-import classes from './MealItemForm.module.css';
+import { useRef, useState } from "react";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+  const submitCartHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+    if (
+      enteredAmount.trim() == 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitCartHandler}>
       <Input
-        label='Amount'
+        ref={amountInputRef}
+        label="Amount"
         input={{
-          id: 'amount_' + props.id,
-          type: 'number',
-          min: '1',
-          max: '5',
-          step: '1',
-          defaultValue: '1',
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Enterd Amount is not Vliad</p>}
     </form>
   );
 };
