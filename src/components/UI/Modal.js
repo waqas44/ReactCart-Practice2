895c350519500798bae36a0df1bb69932b0531de
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-08-24 16:23:12 +0500

    overly close fixed

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,8 +1,8 @@
 import { Fragment } from "react";
 import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
-const Backdrop = () => {
-  return <div className={classes.backdrop} />;
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 const ModalOverlay = (props) => {
   return (
@@ -15,7 +15,7 @@ const overlay = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, overlay)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, overlay)}
       {ReactDOM.createPortal(
         <ModalOverlay> {props.children} </ModalOverlay>,
         overlay
