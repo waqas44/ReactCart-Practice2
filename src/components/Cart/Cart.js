1c2c040c6f6ba0dc3e502032f7a8bcedc48049cf
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-08-24 16:11:39 +0500

    items are getting added into cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,41 @@
+import { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const ctx = useContext(CartContext);
+  const updatedTotalAmount = `$${ctx.totalAmount.toFixed(2)}`;
+
+  const itemAddHandler = (item) => {
+    ctx.addItem({ ...item, amount: 1 });
+  };
+
+  const itemRemoveHandler = (id) => {
+    ctx.removeItem(id);
+  };
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {ctx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onRemove={itemRemoveHandler.bind(null, item.id)}
+          onAdd={itemAddHandler.bind(null, item)}
+        />
+
+        // <li>{item.name}</li>
       ))}
     </ul>
+
+    // <ul className={classes["cart-items"]}>
+    //   {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+    //     <li>{item.name}</li>
+    //   ))}
+    // </ul>
   );
 
   return (
@@ -15,7 +43,7 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{updatedTotalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
